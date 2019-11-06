4be4b8fd25a7ed2d283b7abdd9fb2a6fd3da9f5e
Daniel Varga <daniel.varga@prezi.com>
2019-11-06 02:28:06 +0100

    oracle uncomputation not yet working

--- qc.py
+++ qc.py
@@ -1,11 +1,18 @@
 import sys
+import itertools
 import numpy as np
 
 
-def init(n):
+def init(n, value=None):
+    if value is None:
+        value = [0 for _ in range(n)]
+    else:
+        assert len(value) == n
     v = np.zeros(2 ** n)
-    v[0] = 1
-    return to_cube(v)
+    v = to_cube(v)
+    v[tuple(value)] = 1
+    assert np.sum(v) == 1
+    return v
 
 
 def qubits(v):
@@ -50,6 +57,11 @@ def quantum_not_gate(v, k):
     return apply_quantum_gate(not_matrix, v, [k])
 
 
+def quantum_pauli_z_gate(v, k):
+    pauli_z_matrix = np.array([[1, 0], [0, -1]])
+    return apply_quantum_gate(pauli_z_matrix, v, [k])
+
+
 # bits[k3] is flipped if (bits[k1] and bits[k2]).
 def quantum_toffoli_gate(v, k1, k2, k3):
     toffoli_matrix = np.eye(8)
@@ -66,6 +78,29 @@ def classical_copy_gate(v, k1, k2):
 # bits[k1] = bits[k1] and bits[k2]
 def classical_and_gate(v, k1, k2):
     n = qubits(v)
+
+
+# the quantum version of conditional NOT.
+# clause is a +1 -1 0 vector on the qubits, controlled is one of the qubits.
+# for a basis, if the clause is true, then the controlled qubit is flipped,
+# otherwise it's unaffected.
+def quantum_controlled_pauli_x_gate(v, clause, controlled):
+    assert qubits(v) == len(clause)
+    assert clause[controlled] == 0
+    bits = [i for i, l in enumerate(clause) if l != 0] + [controlled]
+    controlled_pauli_x_matrix = np.eye(2 ** len(bits))
+    controlled_pauli_x_tensor = controlled_pauli_x_matrix.reshape(tuple(2 for _ in range(2 * len(bits))))
+    slicer = [1 if literal == +1 else 0 for literal in clause if literal !=0]
+    controlled_pauli_x_tensor[tuple(slicer + [slice(0, 2)] + slicer + [slice(0, 2)])] = np.array([[0, 1], [1, 0]])
+    return apply_quantum_gate(controlled_pauli_x_tensor, v, bits)
+
+
+def deterministic_get(v):
+    n = qubits(v)
+    assert np.sum(v != 0) == 1
+    for bitvector in itertools.product(*[[0, 1] for _ in range(n)]):
+        if v[tuple(bitvector)] != 0:
+            return bitvector
 
 
 def test_classical_not_gate():
@@ -136,6 +171,19 @@ def test_quantum_toffoli_gate():
         print("=====")
 
 
+# controlled_pauli_x is a generalization of toffoli,
+# so we verify it by comparing it to toffoli in the special case.
+# either that, or we verify toffoli by comparing it to pauli, who cares.
+def test_quantum_controlled_pauli_x_gate():
+    v = init(3)
+    for b1 in (0, 1):
+        for b2 in (0, 1):
+            v = init(3, (b1, b2, 0))
+            vnot = quantum_controlled_pauli_x_gate(v, (+1, +1, 0), 2)
+            vtof = quantum_toffoli_gate(v, 0, 1, 2)
+            assert np.allclose(vnot, vtof)
+
+
 def test_tensordot():
     a = np.arange(12).reshape((2, 2, 3))
     b = np.arange(4).reshape((2, 2))
@@ -144,10 +192,55 @@ def test_tensordot():
     print(np.tensordot(a, b, (1, 0)))
 
 
+# the first n qubits are assumed to be the variables,
+# the second c qubits correspond to the clauses,
+# and are assumed to be initialized to all zeros.
+# the last single qubit is the result of the sat evaluation.
+def apply_sat_circuit(v, clauses):
+    n = len(clauses[0])
+    c = len(clauses)
+    dim = n + c + 1
+    assert dim == qubits(v) # could be more flexible, but it's enough for our purposes
+    pad = [0 for _ in range(c + 1)]
+    for i, clause in enumerate(clauses):
+        v = quantum_controlled_pauli_x_gate(v, clause + pad, n + i)
+    v = quantum_controlled_pauli_x_gate(v, [1 if n <= i < n + c else 0 for i in range(dim)], dim - 1)
+    return v
+
+
+def unapply_sat_circuit(v, clauses):
+    n = len(clauses[0])
+    c = len(clauses)
+    dim = n + c + 1
+    assert dim == qubits(v)
+    pad = [0 for _ in range(c + 1)]
+    v = quantum_controlled_pauli_x_gate(v, [1 if n <= i < n + c else 0 for i in range(dim)], dim - 1)
+    for i, clause in list(enumerate(clauses))[::-1]:
+        v = quantum_controlled_pauli_x_gate(v, clause + pad, n + i)
+    return v
+
+
+def test_sat_circuit():
+    n = 4
+    # (1, 0, 1, 0) is the only assignment satisfying it:
+    clauses = [[+1, -1, +1, 0], [0, 0, +1, -1]]
+    c = len(clauses)
+    dim = n + c + 1
+    pad = [0 for _ in range(c + 1)]
+    for bitvector in itertools.product(*[[0, 1] for _ in range(n)]):
+        v = init(dim, list(bitvector) + pad)
+        v = apply_sat_circuit(v, clauses)
+        v = quantum_pauli_z_gate(v, dim - 1)
+        v = unapply_sat_circuit(v, clauses)
+        print(bitvector, deterministic_get(v))
+
+
 def test():
+    test_sat_circuit()
+    # test_quantum_controlled_pauli_x_gate()
     # test_quantum_toffoli_gate()
     # test_quantum_not_gate()
-    test_quantum_hadamard_gate()
+    # test_quantum_hadamard_gate()
     # test_classical_and_gate()
     # test_tensordot()
     # test_classical_random_bit()
